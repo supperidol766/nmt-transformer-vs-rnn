bfb09e8ddb63bd47e97446d499e55db43bf1a78e
supperidol766 <17703604187@163.com>
2026-02-26 10:36:49 +0000

    Update wrappers.py

--- nmt/models/wrappers.py
+++ nmt/models/wrappers.py
@@ -6,8 +6,9 @@ class EncoderDecoder(nn.Module):
         super().__init__()
         self.encoder = encoder
         self.decoder = decoder
-def forward(self, enc_x: torch.Tensor, dec_x: torch.Tensor, enc_valid_lens: torch.Tensor):
-# encoder outputs: Transformer -> Tensor; Seq2Seq -> (outputs, state)
-    enc_outputs = self.encoder(enc_x, enc_valid_lens)
-    dec_state = self.decoder.init_state(enc_outputs, enc_valid_lens)
-    return self.decoder(dec_x, dec_state)
+    def forward(self, enc_x: torch.Tensor, dec_x: torch.Tensor, enc_valid_lens: torch.Tensor):
+    # encoder outputs: Transformer -> Tensor; Seq2Seq -> (outputs, state)
+        enc_outputs = self.encoder(enc_x, enc_valid_lens)
+        dec_state = self.decoder.init_state(enc_outputs, enc_valid_lens)
+    
+        return self.decoder(dec_x, dec_state)
